4dcb4c33630fa2c9875831ea82a911c69711ad38
Johannes Abraham <johannespabraham@gmail.com>
2025-06-22 22:33:53 +0530

    order button backend

---
+++ models/Order.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const dayMealSchema = new mongoose.Schema({
+    type: String,           // "Breakfast", "Lunch", "Dinner"
+    items: [String]         // List of food items
+})
+
+const orderSchema = new mongoose.Schema({
+    orderId: { type: String, required: true, unique: true },
+    messName: { type: String, required: true },
+    customerName: { type: String, required: true },
+    customerPhone: { type: Number, required: true },
+    orderDate: { type: Date, default: Date.now },
+    status: { type: String, required: true, enum: ['Pending', 'Delivered', 'Cancelled'] }
+})
+
+module.exports = mongoose.model('orders', orderSchema);
